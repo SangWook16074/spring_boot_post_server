584d80ca2cf9d1cc0b98adea284d67aa5610e164
sangwook16074 <qazws78941@gmail.com>
2024-06-19 19:12:36 +0900

    스웨거 파일 적용

--- src/main/kotlin/com/example/msyql_example/member/controller/MemberController.kt
+++ src/main/kotlin/com/example/msyql_example/member/controller/MemberController.kt
@@ -7,6 +7,8 @@ import com.example.msyql_example.member.dto.LoginDto
 import com.example.msyql_example.member.dto.MemberRequestDto
 import com.example.msyql_example.member.dto.MemberResponseDto
 import com.example.msyql_example.member.service.MemberService
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@Tag(name = "회원 Api 컨트롤러", description = "회원가입, 로그인, 내정보 조회 Api 명세서입니다.")
 @RestController
 @RequestMapping("/api/member")
 class MemberController(
@@ -44,6 +47,7 @@ class MemberController(
             .body(BaseResponse(data = tokenInfo))
     }
 
+    @SecurityRequirement(name = "BearerAuth")
     @GetMapping("/info")
     private fun searchMyInfo() : ResponseEntity<BaseResponse<MemberResponseDto>> {
         val id = (SecurityContextHolder.getContext().authentication.principal as CustomUser).id
